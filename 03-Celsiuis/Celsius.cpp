0f453ab272e5f60641e3772aa639562f18c0d890
Facundo Gandolfo <facu.gandolfo@gmail.com>
2019-05-30 20:11:55 -0300

    Update Celsius.cpp

--- 03-Celsiuis/Celsius.cpp
+++ 03-Celsiuis/Celsius.cpp
@@ -1,3 +1,10 @@
+/*
+	Trabajo #3
+    03-Celsius
+	Facundo Gandolfo
+	29/5/2019
+*/
+
 #include <iostream>
 #include <assert.h>
 
@@ -6,18 +13,18 @@ bool AreNear(double, double, double = 0.001);
 
 int main()
 {
-    double Farenheit,
-        x,
-        y,
-        n;
-
     assert(0 == Celsius(32));
+    assert(17 < Celsius(64));
+    std::cout << "Temperatura en celsius: " << Celsius(64) << "\n";
 
-    std::cout << "Temperatura en farenheit: ";
-    std::cin >> Farenheit;
-    std::cout << "Temperatura en celsius: " << Celsius(Farenheit) << "\n";
-    AreNear ? std::cout << "Sí" : std::cout << "No";
+    assert(AreNear(17.7, Celsius(64), .1));
+    assert(AreNear(17.7778, Celsius(64)));
+    assert(AreNear(-17.7778, Celsius(0)));
 }
 
 double Celsius(double f) { return (5.0 / 9.0) * (f - 32); }
-bool AreNear(double x, double y, double n) { return ((x - y) / x) <= n ? true : false; }
+
+bool AreNear(double x, double y, double n)
+{
+    return (x - n) <= y and y <= (x + n);
+}
